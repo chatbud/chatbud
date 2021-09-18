70b0673e482937467c2deee601fe6b074cd2a4aa
Devon Mack <devon.mack@shopify.com>
2021-09-18 15:36:56 -0400

    Backend

--- server/src/Azure.js
+++ server/src/Azure.js
@@ -1,1 +1,11 @@
+import fetch from 'node-fetch';
 
+export async function categorize(query) {
+  const url = process.env.AZURE_URL + query;
+
+  const resp = await fetch(url);
+
+  const json = await resp.json()
+
+  return json;
+}
--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import { Syncrosse} from '@syncrosse/server'
 import express from 'express'
 import http from 'http';
-import fetch from 'node-fetch';
+import { categorize } from './Azure.js';
 
 const app = express();
 const server = http.createServer(app);
@@ -15,15 +15,6 @@ syncrosse.onAction('ping', ({ user, data, lobby }) => {
 });
 
 syncrosse.start();
-async function categorize(query) {
-  const url = process.env.AZURE_URL + query;
-
-  const resp = await fetch(url);
-
-  const json = await resp.json()
-
-  return json;
-}
 
 categorize('animal crossing').then((traits) => console.log(traits));
 
