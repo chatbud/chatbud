cc29bf097e34a20f094938d0e4faeb00d1aa14b9
NamChau Nguyen <namchau.nguyen@shopify.com>
2021-09-18 19:43:39 -0400

    update spacing

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -110,7 +110,7 @@ const IndexPage: NextPage = () => {
             {validationMessage && (
               <div
                 css={[
-                  tw`bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4`
+                  tw`bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4 mt-4`
                 ]}
                 role="alert"
               >
