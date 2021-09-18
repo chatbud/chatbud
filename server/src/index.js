78e32ed539d632e3d34fc7f686dd738595bd7e11
vinhvn <vincentn337@gmail.com>
2021-09-18 18:00:14 -0400

    fix: header auth, login func, routes

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import { Syncrosse } from '@syncrosse/server'
 import express from 'express'
 import http from 'http';
-import bodyParser from 'body-parser';
 import { categorize } from './Azure.js';
 
 // database
@@ -27,30 +26,94 @@ syncrosse.start();
 
 categorize('animal crossing').then((traits) => console.log(traits));
 
+const authenticate = (req, res, next) => {
+  // check headers for id
+  if (!db.users.hasOwnProperty(req.header('User-Id'))) {
+    res.sendStatus(401);
+  } else {
+    next();
+  }
+}
+
+const generatePhoneCode = (phoneNumber) => {
+  const code = Math.floor(Math.random() * 9000) + 1000;
+  phoneCodes[phoneNumber] = code;
+}
+
+/**
+ * Login route (login and sign up)
+ * First sign-in will generate a code
+ */
 app.post('/login', (req, res) => {
-  const { phone_number } = req.body;
+  const { phone_number, code } = req.body;
   if (!phone_number) {
-    req.sendStatus(401);
+    req.sendStatus(400);
     return;
   }
 
-  // twilio api call
-  phoneCodes[phone_number] = '123456';
-  res.sendStatus(200);
-});
-
-app.post('/login/2fa', (req, res) => {
-  const { phone_number, code } = req.body;
-  if (!phone_number || !code) {
+  // generate code if no entry exists
+  if (!phoneCodes.hasOwnProperty(phone_number)) {
+    // twilio api call
+    generatePhoneCode(phone_number);
     res.sendStatus(401);
     return;
   }
-  const verified = code === phoneCodes[phone_number];
-  if (verified) {
-    res.sendStatus(200);
+
+  if (!code) {
+    req.sendStatus(400);
+    return;
+  }
+  // send 200 if code matches, else 401
+  if (code === phoneCodes[phone_number]) {
+    // if user exists, redirect to /home
+    // if not, redirect to /details
+    const id = req.header('User-Id');
+    if (db.users.hasOwnProperty(id)) {
+      res.status.json({ redirect: '/home' });
+    } else {
+      // !TODO: create user entry
+      // db.users[someNewID] = { phone_number };
+      res.status.json({ redirect: '/details' });
+    }
   } else {
     res.sendStatus(401);
   }
+});
+
+/**
+ * Route to update user details
+ */
+app.post('/user/details', authenticate, (req, res) => {
+
+});
+
+/**
+ * Route to update user phone number
+ */
+app.post('/user/phone', authenticate, (req, res) => {
+  // Similar flow to login
+  // generatePhoneCode(phone_number);
+});
+
+/**
+ * Route to get current user's details (plant related?)
+ */
+app.get('/user', authenticate, (req, res) => {
+
+});
+
+/**
+ * Route to get a list of user's buds
+ */
+app.get('/buds', authenticate, (req, res) => {
+
+});
+
+/**
+ * Route to get details for a user's specific bud
+ */
+app.get('/buds/:id', authenticate, (req, res) => {
+
 });
 
 server.listen(process.env.PORT, () => {
