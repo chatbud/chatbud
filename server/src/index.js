3ba1b394dcbe2aeaa94aaee07366c8a1a99c1fec
Devon Mack <devon.mack@shopify.com>
2021-09-18 14:19:03 -0400

    Working server

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,13 @@
 
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
+import { Syncrosse} from '@syncrosse/server'
+import express from 'express'
+import http from 'http';
+import fetch from 'node-fetch';
 
-const { Syncrosse } = require('@syncrosse/server');
-const express = require('express');
 const app = express();
-const http = require('http');
 const server = http.createServer(app);
 const syncrosse = new Syncrosse(server);
-
 dotenv.config();
 
 syncrosse.onAction('ping', ({ user, data, lobby }) => {
@@ -15,6 +15,17 @@ syncrosse.onAction('ping', ({ user, data, lobby }) => {
 });
 
 syncrosse.start();
+async function categorize(query) {
+  const url = process.env.AZURE_URL + query;
+
+  const resp = await fetch(url);
+
+  const json = await resp.json()
+
+  return json;
+}
+
+categorize('animal crossing').then((traits) => console.log(traits));
 
 server.listen(process.env.PORT, () => {
   console.log(`listening on *:${process.env.PORT}`);
