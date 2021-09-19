aff072d8a2b986a733ae69953d780d21f64a7095
deenhaque <fardeen2000@hotmail.com>
2021-09-19 03:17:39 -0400

    Removed unnecessary function import

--- src/pages/details.tsx
+++ src/pages/details.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import tw, { css, styled } from 'twin.macro';
 import TagsInput from 'react-tagsinput';
 import 'react-tagsinput/react-tagsinput.css';
